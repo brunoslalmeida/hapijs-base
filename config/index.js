81244742ed93423b4c3310c8d147c3cb67f37346
Bruno Almeida <bruno@aquelatecnologia.com.br>
2019-09-22 16:49:59 -0300

    adding authorisation to config

--- config/index.js
+++ config/index.js
@@ -1,3 +1,21 @@
+roles = {
+	admin: "admin",
+	user: "user",
+	guest: "guest",
+}
+
+roles.default = roles.user;
+roles.unauth = roles.guest;
+
+exports.Roles = roles;
+
+//The order matters
+exports.Role = [
+	roles.admin,
+	roles.user,
+	roles.guest
+]
+	
 exports.Methods = {
 	GET: "GET",
 	POST: "POST",
